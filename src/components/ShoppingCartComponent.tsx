0f42807391c2f62e6a67c79694447d76bce3d4dc
Kim de Boer <kim-schenk@hotmail.com>
2020-08-20 00:53:24 +0200

    create ShoppingCartComponent

---
+++ src/components/ShoppingCartComponent.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+interface IShoppingCartComponentProps {
+   
+}
+
+export default class ShoppingCartComponent extends React.Component<IShoppingCartComponentProps> {
+    constructor(props: IShoppingCartComponentProps) {
+        super(props)
+    }
+
+    render() {
+        return(
+            <div></div>
+        );
+    }
+}
