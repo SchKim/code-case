27f3cf82ec93425cfc96c5440941ab0358b3be93
Kim de Boer <kim-schenk@hotmail.com>
2020-08-20 00:54:59 +0200

    create headerComponent

---
+++ src/components/HeaderComponent.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+interface IHeaderComponentProps {
+   
+}
+
+export default class ShoppingCartComponent extends React.Component<IHeaderComponentProps> {
+    constructor(props: IHeaderComponentProps) {
+        super(props)
+    }
+
+    render() {
+        return(
+            <div></div>
+        );
+    }
+}
