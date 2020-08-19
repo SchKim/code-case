bf333183d99deb7afa36f392b20f942f8a3218f1
Kim de Boer <kim-schenk@hotmail.com>
2020-08-20 00:56:17 +0200

    create footerComponent

---
+++ src/components/FooterComponent.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+interface IFooterComponentProps {
+   
+}
+
+export default class FooterComponent extends React.Component<IFooterComponentProps> {
+    constructor(props: IFooterComponentProps) {
+        super(props)
+    }
+
+    render() {
+        return(
+            <div></div>
+        );
+    }
+}
