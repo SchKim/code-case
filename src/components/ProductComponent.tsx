d83941d684351a77ccbdede16bfbac07f3a7bd1f
Kim de Boer <kim-schenk@hotmail.com>
2020-08-20 01:12:27 +0200

    create productcomponent

---
+++ src/components/ProductComponent.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+interface IProductComponentProps {
+   
+}
+
+export default class ProductComponent extends React.Component<IProductComponentProps> {
+    constructor(props: IProductComponentProps) {
+        super(props)
+    }
+
+    render() {
+        return(
+            <div></div>
+        );
+    }
+}
