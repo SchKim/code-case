a44172455a016dcfceb9db889a51f8421bba56cd
Kim de Boer <kim-schenk@hotmail.com>
2020-08-21 15:54:30 +0200

    delete styled components

--- src/components/FooterComponent.tsx
+++ src/components/FooterComponent.tsx
@@ -1,14 +1,17 @@
 import React from "react";
 
-interface IFooterComponentProps {
-   
-}
 
-export default class FooterComponent extends React.Component<IFooterComponentProps> {
 
-    render() {
-        return(
-            <h1>FooterComponent</h1>
-        );
-    }
+interface IFooterComponentProps {}
+
+export default class FooterComponent extends React.Component<
+  IFooterComponentProps
+> {
+  render() {
+    return (
+      <div>
+        <h1>FooterComponent</h1>
+      </div>
+    );
+  }
 }
