ecba7fd2c263b1ec49175d48c2440b9d274691f9
Kim Schenk <kimberly.schenk@rabobank.nl>
2020-12-29 20:14:00 +0100

    from class to FunctionalComponennt and onClickhandler

--- src/components/HomeComponent.tsx
+++ src/components/HomeComponent.tsx
@@ -7,22 +7,43 @@ interface IHomeComponentProps {
     clickHandler: (product: IProduct) => void   
 }
 
-export default class HomeComponent extends React.Component<IHomeComponentProps> {
-    render() {  
-        return(
-            <div>
-                {this.renderProducts()}               
-            </div>
-        );
-    }
-    
-    // dit is de For loop 
-    renderProducts() {
+
+export default function HomeComponent(props: IHomeComponentProps){
+// dit is de For loop 
+    const renderProducts = () => {
         // alleen de eerste 10 producten tonen
-        const products = this.props.products.slice(0, 10);
+        const products = props.products.slice(0, 10);
 
         return products.map((product) => {
-            return <ProductComponent key={product.id} product={product} clickHandler={this.props.clickHandler} />
+            return <ProductComponent key={product.id} product={product} clickHandler={props.clickHandler} />
         });
     }    
+
+    return(
+        <div>
+            {renderProducts()}               
+        </div>
+    );
 }
+
+
+
+// export default class HomeComponent extends React.Component<IHomeComponentProps> {
+//     render() {  
+//         return(
+//             <div>
+//                 {this.renderProducts()}               
+//             </div>
+//         );
+//     }
+    
+//     // dit is de For loop 
+//     renderProducts() {
+//         // alleen de eerste 10 producten tonen
+//         const products = this.props.products.slice(0, 10);
+
+//         return products.map((product) => {
+//             return <ProductComponent key={product.id} product={product} clickHandler={this.props.clickHandler} />
+//         });
+//     }    
+// }
